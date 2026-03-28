1f16e3a4a959b6e13082dc93df418d5d8ceca180
Muhammad Arifin <Arifinkembar2@gmail.com>
2026-03-28 14:31:13 +0700

    lib/supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 // lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://dummy.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'dummy-key';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://swdwoezounqobcovdyqw.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3ZHdvZXpvdW5xb2Jjb3ZkeXF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MjE5OTQsImV4cCI6MjA5MDE5Nzk5NH0.hug9BR1dnQ-ljoM35etGJ-IBbsmN48MwrVSkWjdqtIA';
 
 // Inilah jembatan utama kita ke database
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
