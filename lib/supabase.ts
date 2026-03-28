69ae17ce8361c23ce8cd0bbd587a9147f13c33c4
Muhammad Arifin <Arifinkembar2@gmail.com>
2026-03-28 14:26:10 +0700

    fix: update folder dan tambah dummy supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 // lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://dummy.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'dummy-key';
 
 // Inilah jembatan utama kita ke database
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
